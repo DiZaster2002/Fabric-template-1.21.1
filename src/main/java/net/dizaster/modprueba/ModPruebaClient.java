4a25b7df064eb3e5116e511df4cb88e4e0a625f6
DiZaster2002 <carlosdurgon2002@gmail.com>
2025-08-24 14:52:21 +0200

    Initial commit

---
+++ src/main/java/net/dizaster/modprueba/ModPruebaClient.java
@@ -0,0 +1,10 @@
+package net.dizaster.modprueba;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModPruebaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dizaster/modprueba/ModPruebaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dizaster.modprueba;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModPruebaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
